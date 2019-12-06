274dec72f555f8bca594b3260a897d1fe03eac4f
Adel <adelbouhraoua23@gmail.com>
2019-12-06 04:56:24 +0100

    initialisation de npm Ajout de quelques packages Ajout du composant Navbar. Ajout du navbar dans la vue welcome.blade.php temporairement Ajout de custom.scss pour des modifications préprocesseurs css

---
+++ resources/js/components/layout/NavbarComponent.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+    <nav class="navbar navbar-expand-lg bg-transparent">
+        <a class="navbar-brand mr-5" href="#">Logo</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+            aria-expanded="false" aria-label="Toggle navigation"></button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav mx-auto mt-2 mt-lg-0">
+                <li class="nav-item mx-2" v-for="item in links" v-bind:key="item">
+                    <a v-if="!Array.isArray(item)" class="nav-link text-dark hvr-underline-center-out" v-text="item.name" v-bind:href="item.href"> <span class="sr-only">(current)</span></a>
+                </li>
+                
+            </ul>
+            <div class="form-inline">
+                <div class="nav-item border-right pr-2 border-right">
+                    <a name="" id="" class="btn btn-primary rounded-0 text-white p-1" href="#" role="button">S'inscrire</a>
+                </div>
+
+                <div class="nav-item">
+                    <a name="" id="" class="btn btn-link" href="#" role="button">Se Connecter</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</div>
+</template>
+
+<script>
+export default {
+
+    data: function () {  
+        return {
+            links:[
+                {
+                    name:'Accueil',
+                    href:'/'
+                },
+                {
+                    name:'Contact',
+                    href:'/Contact'
+                },
+                {
+                    name:'À propos',
+                    href:'/about'
+                }
+            ]
+        };
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
