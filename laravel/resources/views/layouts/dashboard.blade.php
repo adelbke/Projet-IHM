d9e488ee345de7093de8f758c407fee45483adcf
Quickinline <adelbouhraoua23@gmail.com>
2019-12-30 07:18:05 +0100

    Organisation des blade templates du Dashboard

--- laravel/resources/views/layouts/dashboard.blade.php
+++ laravel/resources/views/layouts/dashboard.blade.php
@@ -5,17 +5,68 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dashboard - @stack('title')</title>
-
+    
     <link rel="stylesheet" href="/css/app.css">
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     @stack('css')
 </head>
 <body>
-
+    
     <div id="app">
-        @yield('content')
-    </div>
+        
+        <div id="wrapper">
 
+            @include('sidebars.dashboard')
+					
+            <!-- Content Wrapper -->
+            <div id="content-wrapper" class="d-flex flex-column">
+                
+                <!-- Main Content -->
+                <div id="content">
+                    @include('navbars.dashboard')
+                    @yield('content')
+                </div>
+                <!-- End of Main Content -->
+                
+                <!-- Footer -->
+                <footer class="sticky-footer bg-white">
+                    <div class="container my-auto">
+                        <div class="copyright text-center my-auto">
+                            <span>Copyright &copy; Your Website 2019</span>
+                        </div>
+                    </div>
+                </footer>
+                <!-- End of Footer -->
+                
+            </div>
+            <!-- End of Content Wrapper -->
+        </div>
+        
+        <!-- Scroll to Top Button-->
+        <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fas fa-angle-up"></i>
+        </a>
+        
+        <!-- Logout Modal-->
+        <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                    <div class="modal-footer">
+                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                        <a class="btn btn-primary" href="login.html">Logout</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
     @stack('js')
     <script src="/js/app.js"></script>
     <script src="js/sb-admin-2.min.js"></script>
