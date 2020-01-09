6cdcd284325c86db72b7f526a1a05343c7579f9d
unknown <meziani.imad98@gmail.com>
2020-01-09 23:58:13 +0100

    changer le other en unknown dans les varibles des statistiques

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -46,7 +46,7 @@ class AppServiceProvider extends ServiceProvider
         });
 
         view()->composer('Dashboard',function($view){
-            
+
             if(auth()->user()->role == "SuperAdmin"){
 
                 // Request Global data
@@ -93,13 +93,13 @@ class AppServiceProvider extends ServiceProvider
 
             $otherLesionsCount = DB::table('lesions')
                 ->join('collections','lesions.collection_id','=','collections.id')
-                ->where('lesions.sex','=','other')
+                ->where('lesions.sex','=','unknown')
                 ->where('collections.user_id','=',auth()->user()->id)->count();
 
         }else{
             $femaleLesionsCount = DB::table('lesions')->where('sex','=','female')->count();
             $maleLesionsCount = DB::table('lesions')->where('sex','=','male')->count();
-            $otherLesionsCount = DB::table('lesions')->where('sex','=','other')->count();
+            $otherLesionsCount = DB::table('lesions')->where('sex','=','unknown')->count();
         }
 
         return collect([$maleLesionsCount,$femaleLesionsCount,$otherLesionsCount]);
