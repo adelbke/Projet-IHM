056028c680e6adf3105f71f89d9209d47b3dea10
unknown <meziani.imad98@gmail.com>
2020-01-12 14:27:03 +0100

    Ajouter la liste de collection au nav bar

--- laravel/resources/views/navbars/dashboard.blade.php
+++ laravel/resources/views/navbars/dashboard.blade.php
@@ -164,12 +164,16 @@
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
                 <a class="dropdown-item" href="/profile">
-                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                    <i class="fas fa-user fa-lm fa-fw mr-2 text-gray-400"></i>
                     Profile
+                </a>
+                <a class="dropdown-item" href="/lesions">
+                    <i class="fas fa-list fa-lm fa-fw mr-2 text-gray-400"></i>
+                   Liste de collections
                 </a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    <i class="fas fa-sign-out-alt fa-lm fa-fw mr-2 text-gray-400"></i>
                     Déconnexion
                 </a>
             </div>
